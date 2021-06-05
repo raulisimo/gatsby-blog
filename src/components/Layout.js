f97fc8162e1512d7dbae8b52a985a87293fca565
Raul <raulraul.sp@gmail.com>
2021-06-05 22:48:43 +0200

    Added blog in Gatsby

---
+++ src/components/Layout.js
@@ -0,0 +1,14 @@
+import React from "react"
+import AppHeader from "./AppHeader"
+import Footer from "./Footer"
+
+export default function Layout({children}) {
+
+  return (
+    <div className="container is-max-desktop">
+      <AppHeader />
+      { children }
+      <Footer />
+    </div>
+  )
+}

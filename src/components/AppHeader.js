6eb58709e57a1296dcd6dc1e0b24982f4bfe681e
Raul <raulraul.sp@gmail.com>
2021-06-06 13:52:49 +0200

    Fixed navbar-burguer button

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from "gatsby";
 
 export default function AppHeader({seo}) {
 
+  const [isActive, setisActive] = useState(false)
+
+  const handleClick = () => {
+    setisActive(!isActive)
+  }
+
+  const handleKeyDown = () => {
+    setisActive(!isActive)
+  }
+
   return (
-    <nav className="navbar is-transparent mb-5 p-5">
-      <div className="navbar-brand">
+    <nav className="navbar is-transparent mb-5 p-5" role="navigation">
+      <div className="navbar-brand" role="button" tabIndex={0} onClick={handleClick} onKeyDown={handleKeyDown}>
         <Link className="navbar-item" to="/">
           <h1 className="title">RAULI_SIMO</h1>
         </Link>
-        <div className="navbar-burger" data-target="navbarExampleTransparentExample">
+        <div className={`navbar-burger burger ${isActive ? "is-active" : ""}`} 
+        data-target="navbarExampleTransparentExample">
           <span></span>
           <span></span>
           <span></span>
         </div>
       </div>
 
-      <div id="navbarExampleTransparentExample" className="navbar-menu">
+      <div id="navbarExampleTransparentExample" className={`navbar-menu ${isActive ? "is-active" : ""}`}>
         <div className="navbar-start">
           <Link className="navbar-item" to="/">
             Home
@@ -24,6 +35,22 @@ export default function AppHeader({seo}) {
           <Link className="navbar-item" to="/blogs">
             Blogs
           </Link>
+          <Link className="navbar-item" to="/about">
+            About
+          </Link>
+          <div className="navbar-item has-dropdown is-hoverable">
+            <Link className="navbar-link" to="/about">
+            Docs
+            </Link>
+            <div className="navbar-dropdown is-boxed">
+              <Link className="navbar-item" to="/about">
+                Overview
+              </Link>
+              <Link className="navbar-item" to="/about">
+                Modifiers
+              </Link>
+            </div>
+          </div>
         </div>
 
         <div className="navbar-end">
